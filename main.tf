193e63f88f0ad6b87ed84f429e3d574a7f965863
dhwarakesh <dhwarakesh31@gmail.com>
2024-11-23 15:45:36 -0500

    .

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "frontend" {
   source = "./modules/app"
   env = var.env
   instance_type = var.instance_type
-  component = frontend
+  component = "frontend"
   ssh_user = var.ssh_user
   ssh_pass = var.ssh_pass
   zone_id = var.zone_id
