77902f668b38b41e973e1e5031d7f7b44c007653
dhwarakesh <dhwarakesh31@gmail.com>
2024-11-27 20:01:52 -0500

    fixed file name

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -22,7 +22,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible ${var.component}.yml -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible expense.yml -e role_name=${var.component}"
     ]
     }
 }
