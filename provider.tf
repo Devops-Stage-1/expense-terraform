83f185b47957699f336b50b698f96f3f64107abc
dhwarakesh <dhwarakesh31@gmail.com>
2024-12-11 16:18:39 -0500

    Updated vault terraform

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address         = "https://vault-internal.dkdevops.online:8200/ui/vault/secrets"
+  address         = "https://vault-internal.dkdevops.online:8200"
   token           = var.vault_token
   skip_tls_verify = true
 }
