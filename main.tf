853afa74843ad5dc2620653dceb07f12acb81274
dhwarakesh <dhwarakesh31@gmail.com>
2024-12-29 18:04:32 -0500

    Fixing error with lb port

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ module "backend" {
   server_app_port_sg_cidr = concat(var.frontend_subnets, var.backend_subnets)
   prometheus_nodes        = var.prometheus_nodes
   lb_app_port_sg_cidr     = var.frontend_subnets
-  lb_port                 = {http: 80}
+  lb_port                 = {http: 8080}
 }
 
 module "mysql" {
