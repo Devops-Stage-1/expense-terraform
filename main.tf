731782d73071d336ee30899234d933e5f1cc2135
dhwarakesh <dhwarakesh31@gmail.com>
2025-01-02 22:41:24 -0500

    enable frontend

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
-# module "frontend" {
-#   depends_on              = [module.backend]
-#
-#   source                  = "./modules/app"
-#   env                     = var.env
-#   instance_type           = var.instance_type
-#   component               = "frontend"
-#   zone_id                 = var.zone_id
-#   vault_token             = var.vault_token
-#   vpc_id                  = module.vpc.vpc_id
-#   subnets                 = module.vpc.frontend_subnets
-#   lb_needed               = true
-#   lb_type                 = "public"
-#   lb_subnets              = module.vpc.public_subnets
-#   app_port                = 80
-#   bastion_nodes           = var.bastion_nodes
-#   server_app_port_sg_cidr = var.public_subnets
-#   prometheus_nodes        = var.prometheus_nodes
-#   lb_app_port_sg_cidr     = ["0.0.0.0/0"]
-#   certificate_arn         = "arn:aws:acm:us-east-1:235494793390:certificate/52ca4c0e-164c-4bbc-9276-82149fdf2e72"
-#   lb_port                 = {http: 80, https: 443}
-# }
-#
+module "frontend" {
+  depends_on              = [module.backend]
+
+  source                  = "./modules/app"
+  env                     = var.env
+  instance_type           = var.instance_type
+  component               = "frontend"
+  zone_id                 = var.zone_id
+  vault_token             = var.vault_token
+  vpc_id                  = module.vpc.vpc_id
+  subnets                 = module.vpc.frontend_subnets
+  lb_needed               = true
+  lb_type                 = "public"
+  lb_subnets              = module.vpc.public_subnets
+  app_port                = 80
+  bastion_nodes           = var.bastion_nodes
+  server_app_port_sg_cidr = var.public_subnets
+  prometheus_nodes        = var.prometheus_nodes
+  lb_app_port_sg_cidr     = ["0.0.0.0/0"]
+  certificate_arn         = "arn:aws:acm:us-east-1:235494793390:certificate/52ca4c0e-164c-4bbc-9276-82149fdf2e72"
+  lb_port                 = {http: 80, https: 443}
+}
+
 module "backend" {
   depends_on    = [module.rds]
 
