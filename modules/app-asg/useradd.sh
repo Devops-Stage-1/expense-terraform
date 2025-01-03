aa19a4f1be4693650b6156932079facacdad0079
dhwarakesh <dhwarakesh31@gmail.com>
2025-01-03 01:05:52 -0500

    Debugging

--- modules/app-asg/useradd.sh
+++ modules/app-asg/useradd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pip3.11 install ansible hvac &>> /tmp/ansible_install.log
+pip3.11 install ansible hvac &>> /opt/ansible_install.log
 
-ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible get-secrets.yml -e vault_token=$vault_token -e component=$component -e env=$env &>> /tmp/secrets.log
-
-ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible expense.yml -e @~/secrets.json -e role_name=$component -e env=$env &>> /tmp/$component.log
-
-rm -f ~/*.json
+#ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible get-secrets.yml -e vault_token=$vault_token -e component=$component -e env=$env &>> /tmp/secrets.log
+#
+#ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible expense.yml -e @~/secrets.json -e role_name=$component -e env=$env &>> /tmp/$component.log
+#
+#rm -f ~/*.json
