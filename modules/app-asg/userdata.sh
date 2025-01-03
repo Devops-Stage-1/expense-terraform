a4d082155173bda0e231b7b41bd0efd0dc6717bd
dhwarakesh <dhwarakesh31@gmail.com>
2025-01-03 02:00:31 -0500

    Debugging

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -2,4 +2,4 @@
 
 pip3.11 install ansible hvac &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible get-secrets.yml -e vault_token=${vault_token} -e component=${component} -e env=${env} &>>/opt/secrets.log
-ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible expense.yml -e @~/secrets.json -e role_name=${component} -e env=${env} &>>/opt/$component.log
+ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible expense.yml -e @~/secrets.json -e role_name=${component} -e env=${env} &>>/opt/${component}.log
