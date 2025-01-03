1b9760d6a50d73b52725e58c2f0ce66d1117b433
dhwarakesh <dhwarakesh31@gmail.com>
2025-01-03 00:57:35 -0500

    fix error

--- modules/app-asg/useradd.sh
+++ modules/app-asg/useradd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible get-secrets.yml -e vault_token=$vault_token -e component=$component -e env=$env
+ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible get-secrets.yml -e vault_token=$vault_token -e component=$component -e env=$env &> /tmp/secrets.log
 
-ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible expense.yml -e @~/secrets.json -e role_name=$component -e env=$env
+ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible expense.yml -e @~/secrets.json -e role_name=$component -e env=$env &> /tmp/$component.log
 
 rm -f ~/secrets.json ~/newrelic.json
