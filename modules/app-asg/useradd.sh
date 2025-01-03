f946cffd92d00950c1b5109d0edad93abd4325b8
dhwarakesh <dhwarakesh31@gmail.com>
2025-01-03 01:02:34 -0500

    fix error

--- modules/app-asg/useradd.sh
+++ modules/app-asg/useradd.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible get-secrets.yml -e vault_token=$vault_token -e component=$component -e env=$env &> /tmp/secrets.log
+pip3.11 install ansible hvac &>> /tmp/ansible_install.log
 
-ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible expense.yml -e @~/secrets.json -e role_name=$component -e env=$env &> /tmp/$component.log
+ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible get-secrets.yml -e vault_token=$vault_token -e component=$component -e env=$env &>> /tmp/secrets.log
 
-rm -f ~/secrets.json ~/newrelic.json
+ansible-pull -i localhost, -U https://github.com/Devops-Stage-1/expense-ansible expense.yml -e @~/secrets.json -e role_name=$component -e env=$env &>> /tmp/$component.log
+
+rm -f ~/*.json
