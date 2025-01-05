3c41983b72de05ba4b8362f363ad5717e9fc896d
dhwarakesh <dhwarakesh31@gmail.com>
2025-01-04 19:07:30 -0500

    Added DNS records for LB

--- modules/app-asg/main.tf
+++ modules/app-asg/main.tf
@@ -52,7 +52,7 @@ resource "aws_route53_record" "lb-record" {
   name    = "${var.component}-${var.env}"
   type    = "CNAME"
   ttl     = 30
-  records = [aws_lb.main[0].dns_name]
+  records = [aws_lb.main.dns_name]
 }
 
 resource "aws_autoscaling_group" "main" {
